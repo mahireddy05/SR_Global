d183e301cd6980b17d4e2b215505ead63415f5ff
Munnangi Mahidhar Reddy <168239459+mahireddy05@users.noreply.github.com>
2025-07-30 08:41:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const session = require('express-session');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // ---------------- MIDDLEWARE ---------------- //
 app.use(express.static(path.join(__dirname, 'public')));
