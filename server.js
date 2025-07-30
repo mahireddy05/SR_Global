e2c8c27f279a58d33f0f554961252a0905b1a12d
mahireddy05 <mahidhar.205@gmail.com>
2025-07-30 10:11:16 +0530

    Revert "Update server.js"
    This reverts commit 95bb0a2eb0f888f9a79b9f310bd843f13055183d.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // server.js
 const express = require('express');
-const mysql = require('mysql');
+const mysql = require('mysq');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const path = require('path');
