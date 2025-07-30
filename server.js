95bb0a2eb0f888f9a79b9f310bd843f13055183d
Munnangi Mahidhar Reddy <168239459+mahireddy05@users.noreply.github.com>
2025-07-30 08:27:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // server.js
 const express = require('express');
-const mysql = require('mysq');
+const mysql = require('mysql');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const path = require('path');
